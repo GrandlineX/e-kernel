0c3f1196cb115675a18b0bb112b94191d4c6a991
Elschnagoo <david.nagy@quinto.io>
2021-12-06 17:16:03 +0100

    v0.16.1

--- src/index.ts
+++ src/index.ts
@@ -10,5 +10,8 @@ export * from './classes';
 export * from './lib';
 export * from './action';
 export * from '@grandlinex/core';
+export * as bundleSQLight from '@grandlinex/bundle-sqlight';
+export * as bundleELogger from '@grandlinex/bundle-elogger';
+export { SQLCon } from '@grandlinex/bundle-sqlight';
 export { ElectronKernelModule, ElectronKernel, KernelDB };
 export default ElectronKernel;
